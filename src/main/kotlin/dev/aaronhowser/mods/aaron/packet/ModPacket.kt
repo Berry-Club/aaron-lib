319ec8218b5257b8144915330cc28e0b3a7e8c6b
aaron <aaronhowser1@gmail.com>
2025-11-13 16:39:29 -0600

    packet stuff

---
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/packet/ModPacket.kt
@@ -0,0 +1,41 @@
+package dev.aaronhowser.mods.aaron.packet
+
+import net.minecraft.core.BlockPos
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.level.ChunkPos
+import net.minecraft.world.phys.Vec3
+import net.neoforged.neoforge.network.PacketDistributor
+import net.neoforged.neoforge.network.handling.IPayloadContext
+
+abstract class ModPacket : CustomPacketPayload {
+
+	protected open fun handleOnClient(context: IPayloadContext) {
+		throw kotlin.UnsupportedOperationException("Packet $this cannot be received on the client!")
+	}
+
+	protected open fun handleOnServer(context: IPayloadContext) {
+		throw kotlin.UnsupportedOperationException("Packet $this cannot be received on the server!")
+	}
+
+	fun receiveOnClient(context: IPayloadContext) {
+		context.enqueueWork {
+			handleOnClient(context)
+		}
+	}
+
+	fun receiveOnServer(context: IPayloadContext) {
+		context.enqueueWork {
+			handleOnServer(context)
+		}
+	}
+
+	fun messagePlayer(player: ServerPlayer) = PacketDistributor.sendToPlayer(player, this)
+	fun messageAllPlayers() = PacketDistributor.sendToAllPlayers(this)
+	fun messageServer() = PacketDistributor.sendToServer(this)
+	fun messageNearbyPlayers(serverLevel: ServerLevel, pos: BlockPos, radius: Double) = messageNearbyPlayers(serverLevel, pos.center, radius)
+	fun messageNearbyPlayers(serverLevel: ServerLevel, origin: Vec3, radius: Double) = PacketDistributor.sendToPlayersNear(serverLevel, null, origin.x, origin.y, origin.z, radius, this)
+	fun messageAllPlayersTrackingChunk(serverLevel: ServerLevel, chunkPos: ChunkPos) = PacketDistributor.sendToPlayersTrackingChunk(serverLevel, chunkPos, this)
+
+}
---
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/packet/ModPacketRegistrar.kt
@@ -0,0 +1,32 @@
+package dev.aaronhowser.mods.aaron.packet
+
+import net.minecraft.network.RegistryFriendlyByteBuf
+import net.minecraft.network.codec.StreamCodec
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload
+import net.neoforged.neoforge.network.registration.PayloadRegistrar
+
+interface ModPacketRegistrar {
+
+	private fun <T : ModPacket> toClient(
+		registrar: PayloadRegistrar,
+		packetType: CustomPacketPayload.Type<T>,
+		streamCodec: StreamCodec<in RegistryFriendlyByteBuf, T>,
+	) {
+		registrar.playToClient(
+			packetType,
+			streamCodec
+		) { packet, context -> packet.receiveOnClient(context) }
+	}
+
+	private fun <T : ModPacket> toServer(
+		registrar: PayloadRegistrar,
+		packetType: CustomPacketPayload.Type<T>,
+		streamCodec: StreamCodec<in RegistryFriendlyByteBuf, T>
+	) {
+		registrar.playToServer(
+			packetType,
+			streamCodec
+		) { packet, context -> packet.receiveOnServer(context) }
+	}
+
+}
