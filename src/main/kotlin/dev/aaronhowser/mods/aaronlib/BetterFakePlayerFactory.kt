bd3f823dde0635f229f9dd32a8995fa0404083ea
aaron <aaronhowser1@gmail.com>
2025-11-13 14:53:21 -0600

    BetterFakePlayerFactory

---
+++ src/main/kotlin/dev/aaronhowser/mods/aaronlib/BetterFakePlayerFactory.kt
@@ -0,0 +1,24 @@
+package dev.aaronhowser.mods.aaronlib
+
+import com.mojang.authlib.GameProfile
+import net.minecraft.server.level.ServerLevel
+import net.neoforged.neoforge.common.util.FakePlayer
+import kotlin.collections.getOrPut
+
+object BetterFakePlayerFactory {
+
+	private val fakePlayers: MutableMap<FakePlayerKey, FakePlayer> = mutableMapOf()
+
+	private data class FakePlayerKey(val level: ServerLevel, val username: GameProfile)
+
+	@JvmStatic
+	fun get(level: ServerLevel, username: GameProfile, create: () -> FakePlayer): FakePlayer {
+		val key = FakePlayerKey(level, username)
+		return fakePlayers.getOrPut(key, create)
+	}
+
+	fun unloadLevel(level: ServerLevel) {
+		fakePlayers.entries.removeIf { it.key.level == level }
+	}
+
+}
