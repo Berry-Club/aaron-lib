134dd8c93158e857482f5fbd2fbef34beb272661
aaron <aaronhowser1@gmail.com>
2025-11-13 16:10:35 -0600

    put in an object

--- src/main/kotlin/dev/aaronhowser/mods/aaron/AaronExtensions.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/AaronExtensions.kt
@@ -11,26 +11,29 @@ import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.DyeColor
 import net.minecraft.world.level.Level
 import net.minecraft.world.phys.Vec3
-import java.util.UUID
+import java.util.*
 
-val Boolean?.isTrue: Boolean
-	inline get() = this == true
+object AaronExtensions {
+	val Boolean?.isTrue: Boolean
+		inline get() = this == true
 
-val Entity.isClientSide: Boolean
-	get() = this.level().isClientSide
+	val Entity.isClientSide: Boolean
+		get() = this.level().isClientSide
 
-val Level.isServerSide: Boolean
-	get() = !this.isClientSide
+	val Level.isServerSide: Boolean
+		get() = !this.isClientSide
 
-fun DyeColor.getDyeName(): String = this.getName()
-fun Direction.getDirectionName(): String = this.getName()
-fun RandomSource.nextRange(min: Float, max: Float): Float = Mth.lerp(nextFloat(), min, max)
-fun RandomSource.nextRange(min: Double, max: Double): Double = Mth.lerp(nextDouble(), min, max)
-fun RandomSource.nextRange(min: Int, max: Int): Int = nextInt(max - min) + min
-fun Player.status(message: Component) = this.displayClientMessage(message, true)
-fun CompoundTag.getUuidOrNull(key: String): UUID? {
-	return if (this.hasUUID(key)) this.getUUID(key) else null
-}
-fun Vec3i.toVec3(): Vec3 {
-	return Vec3(this.x.toDouble(), this.y.toDouble(), this.z.toDouble())
+	fun DyeColor.getDyeName(): String = this.getName()
+	fun Direction.getDirectionName(): String = this.getName()
+	fun RandomSource.nextRange(min: Float, max: Float): Float = Mth.lerp(nextFloat(), min, max)
+	fun RandomSource.nextRange(min: Double, max: Double): Double = Mth.lerp(nextDouble(), min, max)
+	fun RandomSource.nextRange(min: Int, max: Int): Int = nextInt(max - min) + min
+	fun Player.status(message: Component) = this.displayClientMessage(message, true)
+	fun CompoundTag.getUuidOrNull(key: String): UUID? {
+		return if (this.hasUUID(key)) this.getUUID(key) else null
+	}
+
+	fun Vec3i.toVec3(): Vec3 {
+		return Vec3(this.x.toDouble(), this.y.toDouble(), this.z.toDouble())
+	}
 }
