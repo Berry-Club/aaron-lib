3ac87041d1e4e0d50bb774b71bba03cb9b18aa51
aaron <aaronhowser1@gmail.com>
2025-11-13 15:09:46 -0600

    ScreenTextures

---
+++ src/main/kotlin/dev/aaronhowser/mods/aaronlib/menu/ScreenTextures.kt
@@ -0,0 +1,42 @@
+package dev.aaronhowser.mods.aaronlib.menu
+
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.resources.ResourceLocation
+
+object ScreenTextures {
+
+	sealed class Background(
+		private val texture: ResourceLocation,
+		val width: Int,
+		val height: Int,
+		private val canvasSize: Int = 256
+	) {
+//		private val texture = OtherUtil.modResource(path)
+
+		fun render(guiGraphics: GuiGraphics, leftPos: Int, topPos: Int) {
+			guiGraphics.blit(
+				this.texture,
+				leftPos,
+				topPos,
+				0f,
+				0f,
+				this.width,
+				this.height,
+				this.canvasSize,
+				this.canvasSize
+			)
+		}
+
+	}
+
+	sealed class Sprite(
+		private val texture: ResourceLocation,
+		val width: Int,
+		val height: Int
+	) {
+//		val texture = OtherUtil.modResource(path)
+	}
+
+	//TODO: Check that all of these are actually used
+
+}
