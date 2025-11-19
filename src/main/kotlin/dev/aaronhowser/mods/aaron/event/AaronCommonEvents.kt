e4b921290df52253d0bb54a6122b53d65fd42ed3
aaron <aaronhowser1@gmail.com>
2025-11-18 22:55:22 -0600

    Rename AaronServerEvents to AaronCommonEvents and add level tick event handling

--- src/main/kotlin/dev/aaronhowser/mods/aaron/event/AaronCommonEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/event/AaronCommonEvents.kt
@@ -2,13 +2,15 @@ package dev.aaronhowser.mods.aaron.event
 
 import dev.aaronhowser.mods.aaron.AaronLib
 import dev.aaronhowser.mods.aaron.packet.AaronPacketRegister
+import dev.aaronhowser.mods.aaron.scheduler.SchedulerExtensions.getScheduler
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.neoforge.event.entity.player.PlayerEvent
+import net.neoforged.neoforge.event.tick.LevelTickEvent
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent
 
 @EventBusSubscriber(modid = AaronLib.MOD_ID)
-object AaronServerEvents {
+object AaronCommonEvents {
 
 	@SubscribeEvent
 	fun registerPayloads(event: RegisterPayloadHandlersEvent) {
@@ -21,6 +23,11 @@ object AaronServerEvents {
 		if (name.lowercase().contains("aaron")) {
 			AaronLib.LOGGER.info("Aaron spotted!!!!")
 		}
+	}
+
+	@SubscribeEvent
+	fun afterLevelTick(event: LevelTickEvent.Post) {
+		event.level.getScheduler().tick()
 	}
 
 }
