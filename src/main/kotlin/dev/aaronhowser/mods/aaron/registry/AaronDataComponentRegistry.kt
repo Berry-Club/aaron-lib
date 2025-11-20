1570a9214de1e188e6e441a9164e6dad3e371602
aaron <aaronhowser1@gmail.com>
2025-11-20 04:50:25 -0600

    more builders

--- src/main/kotlin/dev/aaronhowser/mods/aaron/registry/AaronDataComponentRegistry.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/registry/AaronDataComponentRegistry.kt
@@ -3,6 +3,7 @@ package dev.aaronhowser.mods.aaron.registry
 import com.mojang.serialization.Codec
 import net.minecraft.core.component.DataComponentType
 import net.minecraft.network.RegistryFriendlyByteBuf
+import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
 import net.minecraft.util.Unit
 import net.neoforged.neoforge.registries.DeferredHolder
@@ -14,6 +15,26 @@ abstract class AaronDataComponentRegistry {
 
 	protected fun unit(name: String): DeferredHolder<DataComponentType<*>, DataComponentType<Unit>> {
 		return register(name, Unit.CODEC, StreamCodec.unit(Unit.INSTANCE))
+	}
+
+	protected fun boolean(name: String): DeferredHolder<DataComponentType<*>, DataComponentType<Boolean>> {
+		return register(name, Codec.BOOL, ByteBufCodecs.BOOL)
+	}
+
+	protected fun int(name: String): DeferredHolder<DataComponentType<*>, DataComponentType<Int>> {
+		return register(name, Codec.INT, ByteBufCodecs.INT)
+	}
+
+	protected fun float(name: String): DeferredHolder<DataComponentType<*>, DataComponentType<Float>> {
+		return register(name, Codec.FLOAT, ByteBufCodecs.FLOAT)
+	}
+
+	protected fun double(name: String): DeferredHolder<DataComponentType<*>, DataComponentType<Double>> {
+		return register(name, Codec.DOUBLE, ByteBufCodecs.DOUBLE)
+	}
+
+	protected fun long(name: String): DeferredHolder<DataComponentType<*>, DataComponentType<Long>> {
+		return register(name, Codec.LONG, ByteBufCodecs.VAR_LONG)
 	}
 
 	protected fun <T> register(
