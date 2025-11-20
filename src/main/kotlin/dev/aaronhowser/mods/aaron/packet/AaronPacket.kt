97e0157f8497a946e31a69bf3d9f8c72298e29e4
aaron <aaronhowser1@gmail.com>
2025-11-20 07:50:52 -0600

    more packet stuff

--- src/main/kotlin/dev/aaronhowser/mods/aaron/packet/AaronPacket.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/packet/AaronPacket.kt
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.entity.Entity
 import net.minecraft.world.level.ChunkPos
 import net.minecraft.world.phys.Vec3
 import net.neoforged.neoforge.network.PacketDistributor
@@ -37,5 +38,7 @@ abstract class AaronPacket : CustomPacketPayload {
 	fun messageNearbyPlayers(serverLevel: ServerLevel, pos: BlockPos, radius: Double) = messageNearbyPlayers(serverLevel, pos.center, radius)
 	fun messageNearbyPlayers(serverLevel: ServerLevel, origin: Vec3, radius: Double) = PacketDistributor.sendToPlayersNear(serverLevel, null, origin.x, origin.y, origin.z, radius, this)
 	fun messageAllPlayersTrackingChunk(serverLevel: ServerLevel, chunkPos: ChunkPos) = PacketDistributor.sendToPlayersTrackingChunk(serverLevel, chunkPos, this)
+	fun messageAllPlayersTrackingEntity(entity: Entity) = PacketDistributor.sendToPlayersTrackingEntity(entity, this)
+	fun messageAllPlayersTrackingEntityAndSelf(entity: Entity) = PacketDistributor.sendToPlayersTrackingEntityAndSelf(entity, this)
 
 }
