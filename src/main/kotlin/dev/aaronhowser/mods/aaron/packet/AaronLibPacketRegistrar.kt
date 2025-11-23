a1ac64f59d40c747dff2f140e26a587c476ff77b
aaron <aaronhowser1@gmail.com>
2025-11-23 08:04:24 -0600

    progress

--- src/main/kotlin/dev/aaronhowser/mods/aaron/packet/AaronLibPacketRegistrar.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/packet/AaronLibPacketRegistrar.kt
@@ -1,9 +1,6 @@
 package dev.aaronhowser.mods.aaron.packet
 
 import dev.aaronhowser.mods.aaron.AaronLib
-import dev.aaronhowser.mods.aaron.packet.c2s.ClientChangedMenuString
-import dev.aaronhowser.mods.aaron.packet.c2s.ClientClickedMenuButton
-import dev.aaronhowser.mods.aaron.packet.s2c.UpdateClientScreenString
 import net.minecraftforge.network.NetworkRegistry
 import net.minecraftforge.network.simple.SimpleChannel
 
@@ -20,31 +17,5 @@ object AaronLibPacketRegistrar : AaronPacketRegistrar() {
 		)
 
 	override fun getChannel(): SimpleChannel = CHANNEL
-
-	fun registerPackets() {
-
-	}
-
-	fun registerPayloads(event: RegisterPayloadHandlersEvent) {
-		val registrar = event.registrar("1")
-
-		toServer(
-			registrar,
-			ClientChangedMenuString.TYPE,
-			ClientChangedMenuString.STREAM_CODEC
-		)
-
-		toServer(
-			registrar,
-			ClientClickedMenuButton.TYPE,
-			ClientClickedMenuButton.STREAM_CODEC
-		)
-
-		toClient(
-			registrar,
-			UpdateClientScreenString.TYPE,
-			UpdateClientScreenString.STREAM_CODEC
-		)
-	}
 
 }
