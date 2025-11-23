053773911894fd33ad7f2adf9426f86ecf2bbbcb
aaron <aaronhowser1@gmail.com>
2025-11-23 08:32:24 -0600

    refactor PseudoDataComponent to use type-based codec management for ItemStack components

--- src/main/kotlin/dev/aaronhowser/mods/aaron/PseudoDataComponent.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/PseudoDataComponent.kt
@@ -6,23 +6,36 @@ import net.minecraft.nbt.NbtOps
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.ItemStack
 
-abstract class PseudoDataComponent(
-	val id: ResourceLocation
-) {
+abstract class PseudoDataComponent<T : PseudoDataComponent.Type> {
 
-	abstract fun <T : PseudoDataComponent> getCodec(): Codec<T>
-
-	companion object {
-		fun ItemStack.hasComponent(id: ResourceLocation): Boolean {
-			return this.tag?.contains(id.toString()).isTrue()
-		}
-
-		fun <T : PseudoDataComponent> ItemStack.saveComponent(component: T) {
-			val codec = component.getCodec<T>()
-			val encoded = codec.encodeStart(NbtOps.INSTANCE, component).getOrThrow(false) {}
-
-			this.orCreateTag.put(component.id.toString(), encoded)
-		}
+	abstract class Type(val id: ResourceLocation) {
+		abstract fun <T : PseudoDataComponent<*>> getCodec(): Codec<T>
 	}
 
+	abstract val type: Type
+
+	companion object {
+		fun ItemStack.hasComponent(type: Type): Boolean {
+			return this.tag?.contains(type.id.toString()).isTrue()
+		}
+
+		fun <T : PseudoDataComponent<*>> ItemStack.saveComponent(component: T) {
+			val codec = component.type.getCodec<T>()
+			val encoded = codec.encodeStart(NbtOps.INSTANCE, component).getOrThrow(false) {}
+
+			this.getOrCreateTag().put(component.type.id.toString(), encoded)
+		}
+
+		fun <T : PseudoDataComponent<T>> ItemStack.loadComponent(id: ResourceLocation, type: Type): T? {
+			val tag = this.tag ?: return null
+			val nbt = tag.get(id.toString()) ?: return null
+
+			val pair = type
+				.getCodec<T>()
+				.decode(NbtOps.INSTANCE, nbt)
+				.getOrThrow(false) {}
+
+			return pair.first
+		}
+	}
 }
