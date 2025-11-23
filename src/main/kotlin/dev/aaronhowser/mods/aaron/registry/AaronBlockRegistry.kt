601c77eaf7de79478687eeceb71ad69106d8d412
aaron <aaronhowser1@gmail.com>
2025-11-23 08:11:31 -0600

    block

--- src/main/kotlin/dev/aaronhowser/mods/aaron/registry/AaronBlockRegistry.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/registry/AaronBlockRegistry.kt
@@ -1,46 +1,47 @@
 package dev.aaronhowser.mods.aaron.registry
 
+import net.minecraft.world.item.BlockItem
+import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.block.TransparentBlock
+import net.minecraft.world.level.block.GlassBlock
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties
-import net.neoforged.neoforge.registries.DeferredBlock
-import net.neoforged.neoforge.registries.DeferredRegister
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.RegistryObject
 
 abstract class AaronBlockRegistry {
 
-	abstract fun getBlockRegistry(): DeferredRegister.Blocks
+	abstract fun getBlockRegistry(): DeferredRegister<Block>
+	protected abstract fun getItemRegistry(): DeferredRegister<Item>
 
-	protected abstract fun getItemRegistry(): DeferredRegister.Items
-
-	protected fun blockWithProperties(name: String, properties: Properties): DeferredBlock<Block> =
+	protected fun blockWithProperties(name: String, properties: Properties): RegistryObject<Block> =
 		registerBlock(name) { Block(properties) }
 
-	protected fun basicBlock(name: String): DeferredBlock<Block> =
+	protected fun basicBlock(name: String): RegistryObject<Block> =
 		blockWithProperties(name, Properties.of())
 
 	protected fun basicGlassBlock(name: String) =
-		registerBlock(name) { TransparentBlock(Properties.ofFullCopy(Blocks.GLASS)) }
+		registerBlock(name) { GlassBlock(Properties.copy(Blocks.GLASS)) }
 
 	protected fun basicCopiedBlock(name: String, blockToCopy: Block) =
-		blockWithProperties(name, Properties.ofFullCopy(blockToCopy))
+		blockWithProperties(name, Properties.copy(blockToCopy))
 
 	protected fun basicStoneBlock(name: String) =
 		basicCopiedBlock(name, Blocks.STONE)
 
-	protected  fun <T : Block> registerBlock(
+	protected fun <T : Block> registerBlock(
 		name: String,
 		supplier: () -> T
-	): DeferredBlock<T> {
+	): RegistryObject<T> {
 		val block = getBlockRegistry().register(name, supplier)
-		getItemRegistry().registerSimpleBlockItem(name, block)
+		getItemRegistry().register(name) { BlockItem(block.get(), Item.Properties()) }
 		return block
 	}
 
 	protected fun <T : Block> registerBlockWithoutItem(
 		name: String,
 		supplier: () -> T
-	): DeferredBlock<T> {
+	): RegistryObject<T> {
 		return getBlockRegistry().register(name, supplier)
 	}
 
