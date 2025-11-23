d149c2e6d165ab667ab6c872352b5c2a21423f57
aaron <aaronhowser1@gmail.com>
2025-11-23 08:52:03 -0600

    menu

--- src/main/kotlin/dev/aaronhowser/mods/aaron/registry/AaronMenuTypesRegistry.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/registry/AaronMenuTypesRegistry.kt
@@ -3,9 +3,9 @@ package dev.aaronhowser.mods.aaron.registry
 import net.minecraft.world.flag.FeatureFlags
 import net.minecraft.world.inventory.AbstractContainerMenu
 import net.minecraft.world.inventory.MenuType
-import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent
-import net.neoforged.neoforge.registries.DeferredHolder
-import net.neoforged.neoforge.registries.DeferredRegister
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.RegistryObject
 import java.util.function.Supplier
 
 abstract class AaronMenuTypesRegistry {
@@ -15,10 +15,14 @@ abstract class AaronMenuTypesRegistry {
 	protected fun <T : AbstractContainerMenu> register(
 		name: String,
 		constructor: MenuType.MenuSupplier<T>
-	): DeferredHolder<MenuType<*>, MenuType<T>> {
-		return getMenuTypeRegistry().register(name, Supplier { MenuType(constructor, FeatureFlags.DEFAULT_FLAGS) })
+	): RegistryObject<MenuType<T>> {
+		return getMenuTypeRegistry()
+			.register(
+				name,
+				Supplier { MenuType(constructor, FeatureFlags.DEFAULT_FLAGS) }
+			)
 	}
 
-	abstract fun registerScreens(event: RegisterMenuScreensEvent)
+	abstract fun registerScreens(event: FMLClientSetupEvent)
 
 }
