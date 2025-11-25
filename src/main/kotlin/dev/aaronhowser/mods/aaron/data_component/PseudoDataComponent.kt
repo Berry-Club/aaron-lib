fc884363f657d857ab6a11d11811dac3c3431330
aaron <aaronhowser1@gmail.com>
2025-11-25 14:25:25 -0600

    rename

--- src/main/kotlin/dev/aaronhowser/mods/aaron/data_component/PseudoDataComponent.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/data_component/PseudoDataComponent.kt
@@ -27,14 +27,14 @@ abstract class PseudoDataComponent<
 			return this.tag?.contains(type.id.toString()).isTrue()
 		}
 
-		fun <C : PseudoDataComponent<C, *>> ItemStack.saveComponent(component: C) {
+		fun <C : PseudoDataComponent<C, *>> ItemStack.setComponent(component: C) {
 			val codec = component.type.getCodec()
 			val encoded = codec.encodeStart(NbtOps.INSTANCE, component).getOrThrow(false) {}
 
 			this.getOrCreateTag().put(component.type.id.toString(), encoded)
 		}
 
-		fun <C : PseudoDataComponent<C, T>, T : Type<C>> ItemStack.loadComponent(type: T): C? {
+		fun <C : PseudoDataComponent<C, T>, T : Type<C>> ItemStack.getComponent(type: T): C? {
 			val tag = this.tag ?: return null
 			val nbt = tag.get(type.id.toString()) ?: return null
 
