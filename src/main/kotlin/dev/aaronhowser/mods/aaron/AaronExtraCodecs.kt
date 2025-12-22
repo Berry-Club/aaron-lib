b62b620f617605431a428b29e67fbe9c164368a3
aaron <aaronhowser1@gmail.com>
2025-12-22 03:10:42 -0600

    add Vector2d codec and stream codec for ByteBuf
    (cherry picked from commit c8bd25d7ae03ed2aa132c453285e76d6b9222f5d)

--- src/main/kotlin/dev/aaronhowser/mods/aaron/AaronExtraCodecs.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/AaronExtraCodecs.kt
@@ -1,6 +1,8 @@
 package dev.aaronhowser.mods.aaron
 
+import com.mojang.serialization.Codec
 import io.netty.buffer.ByteBuf
+import net.minecraft.Util
 import net.minecraft.core.NonNullList
 import net.minecraft.core.Registry
 import net.minecraft.network.codec.ByteBufCodecs
@@ -10,17 +12,19 @@ import net.minecraft.resources.ResourceLocation
 import net.minecraft.tags.TagKey
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.phys.Vec3
+import org.joml.Vector2d
 
 object AaronExtraCodecs {
 
-	val VEC3_STREAM_CODEC: StreamCodec<ByteBuf, Vec3> = object : StreamCodec<ByteBuf, Vec3> {
-		override fun decode(buffer: ByteBuf): Vec3 = Vec3(buffer.readDouble(), buffer.readDouble(), buffer.readDouble())
-		override fun encode(buffer: ByteBuf, value: Vec3) {
-			buffer.writeDouble(value.x)
-			buffer.writeDouble(value.y)
-			buffer.writeDouble(value.z)
+	val VEC3_STREAM_CODEC: StreamCodec<ByteBuf, Vec3> =
+		object : StreamCodec<ByteBuf, Vec3> {
+			override fun decode(buffer: ByteBuf): Vec3 = Vec3(buffer.readDouble(), buffer.readDouble(), buffer.readDouble())
+			override fun encode(buffer: ByteBuf, value: Vec3) {
+				buffer.writeDouble(value.x)
+				buffer.writeDouble(value.y)
+				buffer.writeDouble(value.z)
+			}
 		}
-	}
 
 	val STACK_LIST_STREAM_CODEC: StreamCodec<ByteBuf, NonNullList<ItemStack>> =
 		ByteBufCodecs.fromCodec(NonNullList.codecOf(ItemStack.OPTIONAL_CODEC))
@@ -31,5 +35,24 @@ object AaronExtraCodecs {
 			{ it.location() }
 		)
 	}
+
+	val VECTOR3D_CODEC: Codec<Vector2d> =
+		Codec.DOUBLE
+			.listOf()
+			.comapFlatMap(
+				{ list -> Util.fixedSize(list, 2).map { Vector2d(it[0], it[1]) } },
+				{ vector -> listOf(vector.x, vector.y) }
+			)
+
+	val VECTOR2D_STREAM_CODEC: StreamCodec<ByteBuf, Vector2d> =
+		object : StreamCodec<ByteBuf, Vector2d> {
+			override fun decode(buffer: ByteBuf): Vector2d =
+				Vector2d(buffer.readDouble(), buffer.readDouble())
+
+			override fun encode(buffer: ByteBuf, value: Vector2d) {
+				buffer.writeDouble(value.x)
+				buffer.writeDouble(value.y)
+			}
+		}
 
 }
