ee25753379573e6aaf76564a1c13528444909bae
aaron <aaronhowser1@gmail.com>
2025-12-23 17:47:52 -0600

    defaultBlockState
    (cherry picked from commit db6a1915eeeb0d49d82720b9ce64e0a79ce42497)

--- src/main/kotlin/dev/aaronhowser/mods/aaron/AaronExtensions.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/AaronExtensions.kt
@@ -20,7 +20,9 @@ import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.Ingredient
 import net.minecraft.world.level.ItemLike
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.Vec3
+import net.neoforged.neoforge.registries.DeferredBlock
 import java.util.*
 
 object AaronExtensions {
@@ -82,5 +84,7 @@ object AaronExtensions {
 	fun Style.withClickToSuggestCommand(command: String): Style = withClickEvent(ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command))
 	fun Style.withClickToOpenUrl(url: String): Style = withClickEvent(ClickEvent(ClickEvent.Action.OPEN_URL, url))
 	fun Style.withClickToCopyToClipboard(text: String): Style = withClickEvent(ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, text))
+
+	fun DeferredBlock<*>.defaultBlockState(): BlockState = this.get().defaultBlockState()
 
 }
