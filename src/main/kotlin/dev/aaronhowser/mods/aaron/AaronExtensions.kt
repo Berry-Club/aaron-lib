83929289fb743440d5f5a784e9f9ce851684ed00
aaron <aaronhowser1@gmail.com>
2025-12-30 00:58:43 -0600

    add Entity.isMoving() function and update changelog
    (cherry picked from commit e29ef76e1ff214d95396761b57d8f72fa32f499a)

--- src/main/kotlin/dev/aaronhowser/mods/aaron/AaronExtensions.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/AaronExtensions.kt
@@ -75,6 +75,8 @@ object AaronExtensions {
 	fun ItemLike.asIngredient(): Ingredient = Ingredient.of(this)
 	fun TagKey<Item>.asIngredient(): Ingredient = Ingredient.of(this)
 
+	fun Entity.isMoving(): Boolean = this.deltaMovement.lengthSqr() != 0.0
+
 	fun RandomSource.nextRange(min: Float, max: Float): Float = Mth.lerp(nextFloat(), min, max)
 	fun RandomSource.nextRange(min: Double, max: Double): Double = Mth.lerp(nextDouble(), min, max)
 	fun RandomSource.nextRange(min: Int, max: Int): Int = nextInt(max - min) + min
