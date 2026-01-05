cb3d410ae416fc6b060f59234743720e6c5824e2
aaron <aaronhowser1@gmail.com>
2026-01-05 11:21:31 -0600

    refactor: update AaronMobEffectsRegistry to use Forge's DeferredRegister and RegistryObject

--- src/main/kotlin/dev/aaronhowser/mods/aaron/registry/AaronMobEffectsRegistry.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/registry/AaronMobEffectsRegistry.kt
@@ -1,15 +1,15 @@
 package dev.aaronhowser.mods.aaron.registry
 
 import net.minecraft.world.effect.MobEffect
-import net.neoforged.neoforge.registries.DeferredHolder
-import net.neoforged.neoforge.registries.DeferredRegister
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.RegistryObject
 import java.util.function.Supplier
 
 abstract class AaronMobEffectsRegistry {
 
 	abstract fun getMobEffectRegistry(): DeferredRegister<MobEffect>
 
-	fun <T : MobEffect> register(id: String, supplier: Supplier<T>): DeferredHolder<MobEffect, T> {
+	fun <T : MobEffect> register(id: String, supplier: Supplier<T>): RegistryObject<T> {
 		return getMobEffectRegistry().register(id, supplier)
 	}
 
