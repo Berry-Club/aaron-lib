b0cd905ebb412bd18b50e791d7c25d0dfb8f2dfa
aaron <aaronhowser1@gmail.com>
2026-01-05 18:14:04 -0600

    feat: update AaronAdvancementSubProvider to use new ImpossibleTrigger implementation

--- src/main/kotlin/dev/aaronhowser/mods/aaron/datagen/AaronAdvancementSubProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/datagen/AaronAdvancementSubProvider.kt
@@ -2,25 +2,25 @@ package dev.aaronhowser.mods.aaron.datagen
 
 import net.minecraft.advancements.Advancement
 import net.minecraft.advancements.FrameType
+import net.minecraft.advancements.critereon.ImpossibleTrigger
 import net.minecraft.core.HolderLookup
+import net.minecraft.data.advancements.AdvancementSubProvider
 import net.minecraft.network.chat.Component
 import net.minecraft.world.level.ItemLike
-import net.minecraftforge.common.data.ForgeAdvancementProvider
 import java.util.concurrent.CompletableFuture
 
 abstract class AaronAdvancementSubProvider(
 	val lookupProvider: CompletableFuture<HolderLookup.Provider>
-) : ForgeAdvancementProvider.AdvancementGenerator {
+) : AdvancementSubProvider {
 
 	protected fun advancement(): Advancement.Builder = Advancement.Builder.advancement()
 
-	// FIXME
-//	protected fun Advancement.Builder.addImpossibleCriterion(): Advancement.Builder {
-//		return addCriterion(
-//			"impossible",
-//			CriteriaTriggers.IMPOSSIBLE.createInstance(ImpossibleTrigger.TriggerInstance())
-//		)
-//	}
+	protected fun Advancement.Builder.addImpossibleCriterion(): Advancement.Builder {
+		return addCriterion(
+			"impossible",
+			ImpossibleTrigger.TriggerInstance()
+		)
+	}
 
 	protected fun Advancement.Builder.display(
 		icon: ItemLike,
