14e3ad71c54312cc6d6e45740cca7e94d85d6271
aaron <aaronhowser1@gmail.com>
2026-01-23 15:41:36 -0600

    feat: add StreamCodec for EntitySnapshot with optional fields for Nbt, Flags, and Movement snapshots

--- src/main/kotlin/dev/aaronhowser/mods/aaron/entity/predicate/snapshot/EntitySnapshot.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/entity/predicate/snapshot/EntitySnapshot.kt
@@ -4,11 +4,16 @@ import com.mojang.serialization.Codec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.core.Holder
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
+import net.minecraft.network.RegistryFriendlyByteBuf
+import net.minecraft.network.codec.ByteBufCodecs
+import net.minecraft.network.codec.StreamCodec
 import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
+import java.util.*
 
 data class EntitySnapshot(
 	val entityType: EntityType<*>,
@@ -42,6 +47,32 @@ data class EntitySnapshot(
 						.optionalFieldOf("active_effects", emptyMap())
 						.forGetter(EntitySnapshot::activeEffects)
 				).apply(instance, ::EntitySnapshot)
+			}
+
+		val STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, EntitySnapshot> =
+			StreamCodec.composite(
+				ByteBufCodecs.registry(Registries.ENTITY_TYPE), EntitySnapshot::entityType,
+				ByteBufCodecs.optional(NbtSnapshot.STREAM_CODEC), { Optional.ofNullable(it.nbtSnapshot) },
+				ByteBufCodecs.optional(FlagsSnapshot.STREAM_CODEC), { Optional.ofNullable(it.flagsSnapshot) },
+				ByteBufCodecs.optional(MovementSnapshot.STREAM_CODEC), { Optional.ofNullable(it.movementSnapshot) },
+				ByteBufCodecs.map(
+					::HashMap,
+					ByteBufCodecs.holderRegistry(Registries.MOB_EFFECT),
+					MobEffectInstance.STREAM_CODEC
+				),
+				EntitySnapshot::activeEffects,
+			) { entityType,
+				nbt,
+				flags,
+				movement,
+				effects ->
+				EntitySnapshot(
+					entityType,
+					nbt.orElse(null),
+					flags.orElse(null),
+					movement.orElse(null),
+					effects
+				)
 			}
 
 		fun fromEntity(entity: Entity, includeNbtKeys: List<String>): EntitySnapshot {
