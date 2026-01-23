8991ae0081fe7625e28a6b0ff6f560f459a5523e
aaron <aaronhowser1@gmail.com>
2026-01-23 15:20:10 -0600

    feat: update EntitySnapshot and NbtSnapshot to include optional fields and serialization codecs

--- src/main/kotlin/dev/aaronhowser/mods/aaron/entity/predicate/snapshot/EntitySnapshot.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/entity/predicate/snapshot/EntitySnapshot.kt
@@ -1,21 +1,47 @@
 package dev.aaronhowser.mods.aaron.entity.predicate.snapshot
 
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.core.Holder
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
+import java.util.*
 
 data class EntitySnapshot(
 	val entityType: EntityType<*>,
-	val nbtSnapshot: NbtSnapshot,
-	val flagsSnapshot: FlagsSnapshot,
-	val movementSnapshot: MovementSnapshot,
+	val nbtSnapshot: Optional<NbtSnapshot>,
+	val flagsSnapshot: Optional<FlagsSnapshot>,
+	val movementSnapshot: Optional<MovementSnapshot>,
 	val activeEffects: Map<Holder<MobEffect>, MobEffectInstance>
 ) {
 
 	companion object {
+
+		val CODEC: Codec<EntitySnapshot> =
+			RecordCodecBuilder.create { instance ->
+				instance.group(
+					BuiltInRegistries.ENTITY_TYPE.byNameCodec()
+						.fieldOf("entity_type")
+						.forGetter(EntitySnapshot::entityType),
+					NbtSnapshot.CODEC
+						.optionalFieldOf("nbt")
+						.forGetter(EntitySnapshot::nbtSnapshot),
+					FlagsSnapshot.CODEC
+						.optionalFieldOf("flags")
+						.forGetter(EntitySnapshot::flagsSnapshot),
+					MovementSnapshot.CODEC
+						.optionalFieldOf("movement")
+						.forGetter(EntitySnapshot::movementSnapshot),
+					MobEffectInstance.MAP_CODEC
+						.fieldOf("active_effects")
+						.forGetter(EntitySnapshot::activeEffects)
+				).apply(instance, ::EntitySnapshot)
+			}
+
 		fun fromEntity(entity: Entity, includeNbtKeys: List<String>): EntitySnapshot {
 			val nbtSnapshot = NbtSnapshot.fromEntity(entity, includeNbtKeys)
 
--- src/main/kotlin/dev/aaronhowser/mods/aaron/entity/predicate/snapshot/NbtSnapshot.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/entity/predicate/snapshot/NbtSnapshot.kt
@@ -1,7 +1,11 @@
 package dev.aaronhowser.mods.aaron.entity.predicate.snapshot
 
+import com.mojang.serialization.Codec
+import io.netty.buffer.ByteBuf
 import net.minecraft.advancements.critereon.NbtPredicate
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.codec.ByteBufCodecs
+import net.minecraft.network.codec.StreamCodec
 import net.minecraft.world.entity.Entity
 
 class NbtSnapshot(
@@ -13,6 +17,8 @@ class NbtSnapshot(
 	}
 
 	companion object {
+		val CODEC: Codec<NbtSnapshot> = CompoundTag.CODEC.xmap(::NbtSnapshot, NbtSnapshot::tag)
+		val STREAM_CODEC: StreamCodec<ByteBuf, NbtSnapshot> = ByteBufCodecs.COMPOUND_TAG.map(::NbtSnapshot, NbtSnapshot::tag)
 
 		fun fromEntity(entity: Entity, includeKeys: List<String>): NbtSnapshot {
 			val entityNbt = CompoundTag()
