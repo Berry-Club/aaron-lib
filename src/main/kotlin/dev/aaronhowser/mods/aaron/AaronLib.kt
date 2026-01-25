acd0403207442b2341dd2db036809b6a2742c54b
aaron <aaronhowser1@gmail.com>
2026-01-25 10:28:50 -0600

    Add attribute modifier serialization to AaronLib

--- src/main/kotlin/dev/aaronhowser/mods/aaron/AaronLib.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/AaronLib.kt
@@ -1,10 +1,13 @@
 package dev.aaronhowser.mods.aaron
 
+import com.mojang.serialization.JsonOps
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.entity.ai.attributes.AttributeModifier
 import net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import java.util.*
 
 @Mod(AaronLib.MOD_ID)
 class AaronLib {
@@ -13,6 +16,10 @@ class AaronLib {
 		LOGGER.log(Level.INFO, "Aaron.")
 		LOGGER.log(Level.INFO, "Arriving.")
 		LOGGER.log(Level.INFO, "On the scene.")
+
+		val attributeModifier = AttributeModifier(UUID.randomUUID(), "name", 1.0, AttributeModifier.Operation.ADDITION)
+		val s = AaronExtraCodecs.ATTRIBUTE_MODIFIER_CODEC.encodeStart(JsonOps.INSTANCE, attributeModifier)
+		println(s.getOrThrow(false, {}).toString())
 	}
 
 	companion object {
