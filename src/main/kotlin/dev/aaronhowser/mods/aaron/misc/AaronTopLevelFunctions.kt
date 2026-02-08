6346216f41ae9d92a8dfac96f7f175e1f87d5006
aaron <aaronhowser1@gmail.com>
2026-02-08 00:02:11 -0600

    ugh, fix

--- src/main/kotlin/dev/aaronhowser/mods/aaron/misc/AaronTopLevelFunctions.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/misc/AaronTopLevelFunctions.kt
@@ -4,10 +4,10 @@ import java.util.*
 
 fun <T : Any> weakMutableSet(): MutableSet<T> = Collections.newSetFromMap(WeakHashMap<T, Boolean>())
 
-fun <T> observableMutableSetOf(vararg elements: T, onChange: (Set<T>) -> Unit): MutableSet<T> {
+fun <T> observableMutableSetOf(vararg elements: T, onChange: (Set<T>) -> Unit): ObservableMutableSet<T> {
 	return ObservableMutableSet(mutableSetOf(*elements), onChange)
 }
 
-fun <T> observableMutableSetOf(onChange: (Set<T>) -> Unit): MutableSet<T> {
+fun <T> observableMutableSetOf(onChange: (Set<T>) -> Unit): ObservableMutableSet<T> {
 	return ObservableMutableSet(mutableSetOf(), onChange)
 }
