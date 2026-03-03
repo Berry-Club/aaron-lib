ec9c8ba8bbe01c3b61d5bfa0001be04903c6dd0a
aaron <aaronhowser1@gmail.com>
2026-03-03 06:38:34 -0600

    feat: add ExtractOnlyInvWrapper and ImprovedSimpleContainer classes for enhanced inventory handling

---
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/container/ExtractOnlyInvWrapper.kt
@@ -0,0 +1,11 @@
+package dev.aaronhowser.mods.aaron.container
+
+import net.minecraft.world.Container
+import net.minecraft.world.item.ItemStack
+import net.neoforged.neoforge.items.wrapper.InvWrapper
+
+class ExtractOnlyInvWrapper(inv: Container) : InvWrapper(inv) {
+
+	override fun insertItem(slot: Int, stack: ItemStack, simulate: Boolean): ItemStack = stack
+
+}
