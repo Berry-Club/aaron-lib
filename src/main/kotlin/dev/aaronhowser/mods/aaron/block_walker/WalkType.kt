8d2e7dcc2eb76e69dad0682cb3990bf84ce0a37d
aaron <aaronhowser1@gmail.com>
2026-03-15 22:55:20 -0500

    move back to list, also improve the thing

--- src/main/kotlin/dev/aaronhowser/mods/aaron/block_walker/WalkType.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/block_walker/WalkType.kt
@@ -15,26 +15,28 @@ enum class WalkType(
 
 	;
 
-	val neighborOffsets: Set<Vec3i> = buildSet {
+	val neighborOffsets: List<Vec3i> = buildList {
 		if (!includeDiagonals && !horizontalOnly) {
 			for (dir in Direction.entries) {
 				add(Vec3i(dir.stepX, dir.stepY, dir.stepZ))
 			}
-		} else {
-			val verticalRange = if (horizontalOnly) 0..0 else -1..1
 
-			for (dy in verticalRange) {
+			return@buildList
+		}
 
-				if (includeDiagonals) {
-					for (dx in -1..1) for (dz in -1..1) {
-						if (dx == 0 && dy == 0 && dz == 0) continue
-						add(Vec3i(dx, dy, dz))
-					}
-				} else {
-					for (dir in Direction.Plane.HORIZONTAL) {
-						add(Vec3i(dir.stepX, dy, dir.stepZ))
-					}
+		val verticalRange = if (horizontalOnly) 0..0 else -1..1
+		for (dy in verticalRange) {
+			if (includeDiagonals) {
+				for (dx in -1..1) for (dz in -1..1) {
+					if (dx == 0 && dy == 0 && dz == 0) continue
+					add(Vec3i(dx, dy, dz))
 				}
+
+				continue
+			}
+
+			for (dir in Direction.Plane.HORIZONTAL) {
+				add(Vec3i(dir.stepX, dy, dir.stepZ))
 			}
 		}
 	}
