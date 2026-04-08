4436a78d208c4b997c3cdc1159bf4e3c3540efa4
aaron <aaronhowser1@gmail.com>
2026-04-08 05:52:25 -0500

    refactor: move utility functions to companion object in AaronAdvancementSubProvider

--- src/main/kotlin/dev/aaronhowser/mods/aaron/datagen/AaronAdvancementSubProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/datagen/AaronAdvancementSubProvider.kt
@@ -14,34 +14,36 @@ abstract class AaronAdvancementSubProvider(
 	val lookupProvider: CompletableFuture<HolderLookup.Provider>
 ) : AdvancementProvider.AdvancementGenerator {
 
-	protected fun advancement(): Advancement.Builder = Advancement.Builder.advancement()
+	companion object {
+		protected fun advancement(): Advancement.Builder = Advancement.Builder.advancement()
 
-	protected fun Advancement.Builder.addImpossibleCriterion(): Advancement.Builder {
-		return addCriterion(
-			"impossible",
-			CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
-		)
-	}
+		fun Advancement.Builder.addImpossibleCriterion(): Advancement.Builder {
+			return addCriterion(
+				"impossible",
+				CriteriaTriggers.IMPOSSIBLE.createCriterion(ImpossibleTrigger.TriggerInstance())
+			)
+		}
 
-	protected fun Advancement.Builder.display(
-		icon: ItemLike,
-		title: Component,
-		description: Component,
-		type: AdvancementType = AdvancementType.TASK,
-		showToast: Boolean = true,
-		announceToChat: Boolean = true,
-		hidden: Boolean = false
-	): Advancement.Builder {
-		return display(
-			icon,
-			title,
-			description,
-			null,
-			type,
-			showToast,
-			announceToChat,
-			hidden
-		)
+		fun Advancement.Builder.display(
+			icon: ItemLike,
+			title: Component,
+			description: Component,
+			type: AdvancementType = AdvancementType.TASK,
+			showToast: Boolean = true,
+			announceToChat: Boolean = true,
+			hidden: Boolean = false
+		): Advancement.Builder {
+			return display(
+				icon,
+				title,
+				description,
+				null,
+				type,
+				showToast,
+				announceToChat,
+				hidden
+			)
+		}
 	}
 
 }
