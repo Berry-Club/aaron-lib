0929d5abbbbae83344f0e480e13d6f4afe9e3e14
aaron <aaronhowser1@gmail.com>
2026-04-09 01:20:07 -0500

    comment out more

--- src/main/kotlin/dev/aaronhowser/mods/aaron/menu/components/ImprovedSpriteButton.kt
+++ src/main/kotlin/dev/aaronhowser/mods/aaron/menu/components/ImprovedSpriteButton.kt
@@ -10,72 +10,72 @@ import net.minecraft.network.chat.Component
 import net.minecraft.resources.Identifier
 import net.minecraft.util.Mth
 
-open class ImprovedSpriteButton(
-	x: Int = 0,
-	y: Int = 0,
-	width: Int,
-	height: Int,
-	private val spriteWidth: Int,
-	private val spriteHeight: Int,
-	private val sprite: Identifier,
-	onPress: OnPress,
-	message: Component = Component.empty(),
-	private val font: Font
-) : Button(x, y, width, height, message, onPress, DEFAULT_NARRATION) {
-
-	constructor(
-		x: Int,
-		y: Int,
-		width: Int,
-		height: Int,
-		menuSprite: ScreenSprite,
-		onPress: OnPress,
-		message: Component = Component.empty(),
-		font: Font
-	) : this(x, y, width, height, menuSprite.width, menuSprite.height, menuSprite.texture, onPress, message, font)
-
-	override fun renderWidget(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
-		baseRenderWidget(guiGraphics, mouseX, mouseY, partialTick)
-
-		val i = this.x + this.getWidth() / 2 - this.spriteWidth / 2
-		val j = this.y + this.getHeight() / 2 - this.spriteHeight / 2
-		guiGraphics.blitSprite(
-			sprite,
-			i,
-			j,
-			this.spriteWidth,
-			this.spriteHeight
-		)
-
-		if (isMouseOver(mouseX.toDouble(), mouseY.toDouble())) {
-			renderToolTip(guiGraphics, mouseX, mouseY)
-		}
-	}
-
-	private fun renderToolTip(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int) {
-		if (this.message == Component.empty()) return
-
-		guiGraphics.renderComponentTooltip(
-			font,
-			listOf(this.message),
-			mouseX,
-			mouseY
-		)
-	}
-
-	override fun renderString(guiGraphics: GuiGraphics, font: Font, color: Int) {
-		// Do nothing
-	}
-
-	private fun baseRenderWidget(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
-		val minecraft = Minecraft.getInstance()
-		guiGraphics.setColor(1.0f, 1.0f, 1.0f, this.alpha)
-		RenderSystem.enableBlend()
-		RenderSystem.enableDepthTest()
-		guiGraphics.blitSprite(SPRITES[this.active, this.isHovered], this.x, this.y, this.getWidth(), this.getHeight())
-		guiGraphics.setColor(1.0f, 1.0f, 1.0f, 1.0f)
-		val i = fgColor
-		this.renderString(guiGraphics, minecraft.font, i or (Mth.ceil(this.alpha * 255.0f) shl 24))
-	}
-
-}
+//open class ImprovedSpriteButton(
+//	x: Int = 0,
+//	y: Int = 0,
+//	width: Int,
+//	height: Int,
+//	private val spriteWidth: Int,
+//	private val spriteHeight: Int,
+//	private val sprite: Identifier,
+//	onPress: OnPress,
+//	message: Component = Component.empty(),
+//	private val font: Font
+//) : Button(x, y, width, height, message, onPress, DEFAULT_NARRATION) {
+//
+//	constructor(
+//		x: Int,
+//		y: Int,
+//		width: Int,
+//		height: Int,
+//		menuSprite: ScreenSprite,
+//		onPress: OnPress,
+//		message: Component = Component.empty(),
+//		font: Font
+//	) : this(x, y, width, height, menuSprite.width, menuSprite.height, menuSprite.texture, onPress, message, font)
+//
+//	override fun renderWidget(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
+//		baseRenderWidget(guiGraphics, mouseX, mouseY, partialTick)
+//
+//		val i = this.x + this.getWidth() / 2 - this.spriteWidth / 2
+//		val j = this.y + this.getHeight() / 2 - this.spriteHeight / 2
+//		guiGraphics.blitSprite(
+//			sprite,
+//			i,
+//			j,
+//			this.spriteWidth,
+//			this.spriteHeight
+//		)
+//
+//		if (isMouseOver(mouseX.toDouble(), mouseY.toDouble())) {
+//			renderToolTip(guiGraphics, mouseX, mouseY)
+//		}
+//	}
+//
+//	private fun renderToolTip(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int) {
+//		if (this.message == Component.empty()) return
+//
+//		guiGraphics.renderComponentTooltip(
+//			font,
+//			listOf(this.message),
+//			mouseX,
+//			mouseY
+//		)
+//	}
+//
+//	override fun renderString(guiGraphics: GuiGraphics, font: Font, color: Int) {
+//		// Do nothing
+//	}
+//
+//	private fun baseRenderWidget(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
+//		val minecraft = Minecraft.getInstance()
+//		guiGraphics.setColor(1.0f, 1.0f, 1.0f, this.alpha)
+//		RenderSystem.enableBlend()
+//		RenderSystem.enableDepthTest()
+//		guiGraphics.blitSprite(SPRITES[this.active, this.isHovered], this.x, this.y, this.getWidth(), this.getHeight())
+//		guiGraphics.setColor(1.0f, 1.0f, 1.0f, 1.0f)
+//		val i = fgColor
+//		this.renderString(guiGraphics, minecraft.font, i or (Mth.ceil(this.alpha * 255.0f) shl 24))
+//	}
+//
+//}
