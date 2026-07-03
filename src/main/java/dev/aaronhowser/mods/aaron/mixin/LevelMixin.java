9b9905e07da871528a3d49de1c45d80d7c7753b7
aaron <aaronhowser1@gmail.com>
2026-07-03 09:50:47 -0500

    fixin mixin

--- src/main/java/dev/aaronhowser/mods/aaron/mixin/LevelMixin.java
+++ src/main/java/dev/aaronhowser/mods/aaron/mixin/LevelMixin.java
@@ -7,7 +7,6 @@ import dev.aaronhowser.mods.aaron.scheduler.ScheduledTaskHandler;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 import java.util.ArrayList;
@@ -22,13 +21,10 @@ public abstract class LevelMixin implements SchedulerHolder, LevelActorHolder {
 	@Unique
 	private final List<LevelActor> aaron$levelActors = new ArrayList<>();
 
-	@Shadow
-	public abstract long getGameTime();
-
 	@Override
 	public ScheduledTaskHandler aaron$getScheduledTaskHandler() {
 		if (aaron$scheduledTaskHandler == null) {
-			aaron$scheduledTaskHandler = new ScheduledTaskHandler(this::getGameTime);
+			aaron$scheduledTaskHandler = new ScheduledTaskHandler(((Level) (Object) this)::getGameTime);
 		}
 
 		return aaron$scheduledTaskHandler;
